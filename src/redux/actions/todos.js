d78e297d7761b52df0cda7445f82d63e0f17520e
Dan <danpgill@gmail.com>
2017-04-20 12:07:20 +0100

    Converted actions in todoList to use redux-thunk

--- src/redux/actions/todos.js
+++ src/redux/actions/todos.js
@@ -1,15 +1,16 @@
-export const addTodo = (todo) => {
-  return { type: 'todos/ADD_TODO', payload: todo }
+export const addTodo = (todo) => (dispatch, getState) => {
+  _.delay(() => dispatch({ type: 'todos/ADD_TODO', payload: todo }), 2000)
 }
 
-export const updateTodoText = (todoText) => {
-  return { type: 'todos/UPDATE_TODO_TEXT', payload: todoText }
+export const updateTodoText = (todoText) => (dispatch, getState) => {
+  dispatch({ type: 'todos/UPDATE_TODO_TEXT', payload: todoText })
 }
 
-export const completeTodo = (todoId) => {
-  return { type: 'todos/COMPLETE_TODO', payload: todoId }
+export const completeTodo = (todoId) => (dispatch, getState) => {
+  dispatch({ type: 'todos/COMPLETE_TODO', payload: todoId })
+  dispatch({ type: 'todos/COMPLETE_TODO', payload: todoId })
 }
 
-export const deleteTodo = (todoId) => {
-  return { type: 'todos/DELETE_TODO', payload: todoId }
+export const deleteTodo = (todoId) => (dispatch, getState) => {
+  dispatch({ type: 'todos/DELETE_TODO', payload: todoId })
 }
